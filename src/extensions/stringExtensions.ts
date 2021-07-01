38ab61c6f2a162c33e7f9cea19df5eba72d6b3de
Jack Hermanson <jack.hermanson@live.com>
2021-07-01 11:48:39 -0600

    add pluralize

--- src/extensions/stringExtensions.ts
+++ src/extensions/stringExtensions.ts
@@ -4,12 +4,25 @@ declare global {
          * @description - Capitalize the first letter of a string.
          */
         capitalizeFirst(): string;
+
+        /**
+         * @description - Make a string plural depending on the number.
+         *
+         * @param count - The number used to determine if it is plural.
+         * @param ending - The suffix added to the word to make it plural.
+         */
+        pluralize(count: number, ending?: string): string;
     }
 }
 
 String.prototype.capitalizeFirst = function() {
-    let string = String(this);
-    return `${string.charAt(0).toUpperCase()}${string.substr(1)}`;
+    let someString = String(this);
+    return `${someString.charAt(0).toUpperCase()}${someString.substr(1)}`;
+}
+
+String.prototype.pluralize = function(count, ending = "s") {
+    let someString = String(this);
+    return `${someString}${count !== 1 ? ending : ""}`;
 }
 
 export {};
