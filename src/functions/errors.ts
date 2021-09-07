5b0851a62555f6e304c4935c4beb10169ea9d03f
Jack Hermanson <jack.hermanson@live.com>
2021-09-06 23:27:23 -0600

    add conflictError

--- src/functions/errors.ts
+++ src/functions/errors.ts
@@ -5,3 +5,19 @@ export const sendError = (error: Error, res: Response) => {
     console.error(error);
     res.status(HTTP.SERVER_ERROR).json(error);
 };
+
+/**
+ * Returns a string for error messages when you get a 409.
+ * @param conflictingProperties - Comes from the response.
+ * @param name - The name of the type of item you attempted to create/edit.
+ * @param useAn - Use "an" instead of "a"; i.e., name begins with a vowel.
+ */
+export const conflictError = (
+    conflictingProperties: string[],
+    name?: string,
+    useAn?: boolean
+): string => {
+    return `A${useAn && "n"} ${
+        name ? name : "record"
+    } already exists with the same ${conflictingProperties.join(", ")}.`;
+};
